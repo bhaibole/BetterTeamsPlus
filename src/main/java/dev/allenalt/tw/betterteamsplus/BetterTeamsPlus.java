e35d8b7ac3dbfe2dc4471892a5f60cd29b5f808b
bhaibole <luxurystyle8558@gmail.com>
2025-10-27 10:44:19 +0530

    Update BetterTeamsPlus.java

--- src/main/java/dev/allenalt/tw/betterteamsplus/BetterTeamsPlus.java
+++ src/main/java/dev/allenalt/tw/betterteamsplus/BetterTeamsPlus.java
@@ -1,16 +1,39 @@
 package dev.allenalt.tw.betterteamsplus;
 
+import dev.allenalt.tw.betterteamsplus.commands.TeamCommand;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
 
 public class BetterTeamsPlus extends JavaPlugin {
 
+    private static BetterTeamsPlus instance;
+    public static BetterTeamsPlus getInstance() { return instance; }
+
     @Override
     public void onEnable() {
-        getLogger().info("BetterTeamsPlus has been enabled!");
+        instance = this;
+
+        saveDefaultConfig();
+        saveMessages();
+
+        getCommand("team").setExecutor(new TeamCommand());
+        getLogger().info("BetterTeamsPlus v1.1 enabled with GUI & custom config support!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("BetterTeamsPlus has been disabled.");
+        getLogger().info("BetterTeamsPlus disabled.");
+    }
+
+    private void saveMessages() {
+        File msgFile = new File(getDataFolder(), "messages.yml");
+        if (!msgFile.exists()) {
+            saveResource("messages.yml", false);
+        }
+    }
+
+    public FileConfiguration getMessages() {
+        return YamlConfiguration.loadConfiguration(new File(getDataFolder(), "messages.yml"));
     }
 }
